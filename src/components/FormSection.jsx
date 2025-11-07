ea5fbef471e2b61cdf3323f3189694849119fc94
Le Nguyen Tri Nguyen <nguyen081104@gmail.com>
2025-11-08 02:37:54 +0700

    CSS form-insert

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -3,6 +3,8 @@ import "./FormSection.css";
 
 function FormSection() {
   const [formData, setFormData] = useState({
+    lastName: "",
+    middleAndFirstName: "",
     fullName: "",
     email: "",
     phone: "",
@@ -14,6 +16,7 @@ function FormSection() {
   // Handle input change
   const handleChange = (e) => {
     const { name, value } = e.target;
+    // name is "lastName", or "middleAndFirstName"
     setFormData({ ...formData, [name]: value });
   };
 
@@ -24,23 +27,55 @@ function FormSection() {
     alert("Đã gửi đơn thành công!");
   };
 
+  const fullNameCalculated = (
+    formData.lastName +
+    " " +
+    formData.middleAndFirstName
+  ).trim();
+
   return (
     <section className="form-section">
       <h2>Nhập Thông Tin</h2>
       <p>Vui lòng điền đầy đủ thông tin dưới đây theo biểu mẫu</p>
 
       <form onSubmit={handleSubmit}>
-        <label>
-          Họ và Tên <span>*</span>
-          <input
-            type="text"
-            name="fullName"
-            placeholder="Nhập họ và tên đầy đủ"
-            value={formData.fullName}
-            onChange={handleChange}
-            required
-          />
-        </label>
+        <div className="form-name">
+          <label>
+            Họ<span>*</span>
+            <input
+              type="text"
+              name="lastName"
+              placeholder="Nhập họ"
+              value={formData.lastName}
+              onChange={handleChange}
+              required
+            />
+          </label>
+
+          <label>
+            Tên đệm và Tên <span>*</span>
+            <input
+              type="text"
+              name="middleAndFirstName"
+              placeholder="Nhập tên đệm và tên"
+              value={formData.middleAndFirstName}
+              onChange={handleChange}
+              required
+            />
+          </label>
+
+          <label>
+            Họ và Tên <span>*</span>
+            <input
+              type="text"
+              name="fullName"
+              placeholder="Họ và tên"
+              value={fullNameCalculated} //Gán value đã được tính toán
+              readOnly //Người dùng không thể chỉnh sửa trực tiếp
+              required
+            />
+          </label>
+        </div>
 
         <label>
           Email <span>*</span>
@@ -67,7 +102,45 @@ function FormSection() {
         </label>
 
         <label>
-          Địa Chỉ <span>*</span>
+          Địa chỉ người gửi/ From <span>*</span>
+          <input
+            type="text"
+            name="address"
+            placeholder="Nhập địa chỉ"
+            value={formData.address}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <div className="form-row">
+          <label>
+            Thành Phố <span>*</span>
+            <input
+              type="text"
+              name="city"
+              placeholder="Nhập thành phố"
+              value={formData.city}
+              onChange={handleChange}
+              required
+            />
+          </label>
+
+          <label>
+            Quận/Huyện <span>*</span>
+            <input
+              type="text"
+              name="district"
+              placeholder="Nhập quận/huyện"
+              value={formData.district}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+
+        <label>
+          Địa chỉ người nhận/ To <span>*</span>
           <input
             type="text"
             name="address"
